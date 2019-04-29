3a185417fd1cba6b9ca980aae6cc1aed80e8c018
Salman Khan <36618096+slmnkhan786@users.noreply.github.com>
2019-04-29 15:35:48 +0530

    Add files via upload

---
+++ LinkedListL/Client.java
@@ -0,0 +1,38 @@
+package LinkedListL;
+
+
+public class Client {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		LinkedList list = new LinkedList();
+
+//		list.addLast(10);
+//		list.addLast(20);
+//		list.addLast(50);
+//		list.addLast(60);
+//		list.addLast(70);
+//		list.addLast(30);
+//		list.addLast(40);
+		
+//		System.out.println(list.removeLast());
+       //list.display();
+        
+//		list.reverseDRHeapMover();
+	//list.display();
+//        list.reversePR();
+//        list.display();
+		
+//		System.out.println(list.mid());
+//		
+//		System.out.println(list.kthFromLast(2));
+//	list.kreverse(3);
+//	list.display();
+//list.mergeSort();
+		list.createdummyLL();
+		//list.removeLoop();
+list.display();
+	}
+
+	
+}
---
+++ LinkedListL/LinkedList.java
@@ -0,0 +1,442 @@
+package LinkedListL;
+
+import L9_sep16.Queue;
+import L_18.BinaryTree.Node;
+
+public class LinkedList {
+	private class Node {
+		int data;
+		Node next;
+	}
+
+	private Node head;
+	private Node tail;
+	private int size;
+
+	// get first node data
+	public int getFirst() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		return this.head.data;
+	}
+
+	// get last node data
+	public int getLast() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		return this.tail.data;
+	}
+
+	// get data of particular node
+	public int getAt(int index) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		if (index < 0 || this.size <= index) {
+			throw new Exception("Invalid index");
+		}
+		Node temp = head;
+		for (int i = 1; i <= index; i++)
+			temp = temp.next;
+		return temp.data;
+	}
+
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	// Adding new node in LL at last
+	public void addLast(int item) {
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		if (size > 0)
+			this.tail.next = nn;
+		if (this.size == 0) {
+			this.head = nn;
+		}
+		this.tail = nn;
+		this.size++;
+	}
+
+	// Getting a node of given index
+	private Node getNodeAt(int index) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		if (index < 0 || this.size <= index) {
+			throw new Exception("Invalid index");
+		}
+		Node temp = head;
+		for (int i = 1; i <= index; i++)
+			temp = temp.next;
+		return temp;
+	}
+
+	// Adding new node in LL at first
+	public void addFirst(int item) {
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		if (size > 0)
+			nn.next = this.head;
+		if (this.size == 0) {
+			this.head = nn;
+			this.tail = nn;
+		}
+		this.head = nn;
+		this.size++;
+	}
+
+	// Adding new node in LL at a given position
+	public void addAt(int idx, int item) throws Exception {
+
+		if (idx < 0 || this.size <= idx) {
+			throw new Exception("Invalid index");
+		}
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		if (this.size == 0) {
+			this.addFirst(item);
+		} else if (this.size == idx) {
+			this.addLast(item);
+		} else {
+			Node nm1 = this.getNodeAt(idx - 1);
+			nn.next = nm1.next.next;
+			nm1.next = nn;
+			this.size++;
+		}
+	}
+
+	// Removing first node of LL
+	public int removeFirst() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		int temp = this.head.data;
+		if (this.size == 1) {
+			this.head = null;
+			this.tail = null;
+			this.size = 0;
+		} else {
+			head = head.next;
+			this.size--;
+		}
+		return temp;
+	}
+
+	// Removing last node of LL
+	public int removeLast() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		int temp = this.tail.data;
+		if (this.size == 1) {
+			this.head = null;
+			this.tail = null;
+			this.size = 0;
+		} else {
+			Node ptr = this.getNodeAt(this.size - 2);
+			ptr.next = null;
+			this.tail = ptr;
+			this.size--;
+		}
+		return temp;
+	}
+
+	// Removing a given index node of LL
+	public int removeAt(int idx) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+		if (idx < 0 || this.size <= idx) {
+			throw new Exception("Invalid index");
+		}
+		Node temp = null;
+		if (idx == 0) {
+			return this.removeFirst();
+		} else if (this.size - 1 == idx) {
+			return this.removeLast();
+		} else {
+			Node nm1 = this.getNodeAt(idx - 1);
+			temp = nm1.next;
+			nm1.next = nm1.next.next;
+
+			this.size--;
+
+		}
+		return temp.data;
+
+	}
+
+	// Reversing LL Iteratively by swapping node data
+	public void reverseDI() {
+
+		Node temp1 = this.head;
+		Node temp2 = null;
+		while (temp1.next != null) {
+			temp2 = temp1.next;
+			while (temp2 != null) {
+				int temp = temp1.data;
+				temp1.data = temp2.data;
+				temp2.data = temp;
+				temp2 = temp2.next;
+			}
+			temp1 = temp1.next;
+		}
+
+	}
+
+	// Reversing LL Iteratively
+	public void reversePI() {
+		Node temp1 = this.head;
+		Node temp2 = this.head.next;
+		while (temp2 != null) {
+			Node temp = temp2.next;
+			temp2.next = temp1;
+			temp1 = temp2;
+			temp2 = temp;
+
+		}
+		Node temp = this.head;
+		this.head = this.tail;
+		this.tail = temp;
+		this.tail.next = null;
+	}
+
+	public void reversePR() {
+		PRH(this.head, this.head.next);
+		Node temp = this.head;
+		this.head = this.tail;
+		this.tail = temp;
+		this.tail.next = null;
+	}
+
+	private void PRH(Node prv, Node cur) {
+		if (cur == null)
+			return;
+		PRH(cur, cur.next);
+		cur.next = prv;
+	}
+
+	public void reverseDRReturn() {
+
+		DRReturn(this.head, this.head, 0);
+
+	}
+
+	private Node DRReturn(Node left, Node right, int count) {
+		if (right == null)
+			return left;
+
+		left = DRReturn(left, right.next, count + 1);
+		if (count >= size / 2) {
+			int temp = left.data;
+			left.data = right.data;
+			right.data = temp;
+		}
+		return left.next;
+	}
+
+	private class HeapMover {
+		private Node left = head;
+	}
+
+	public void reverseDRHeapMover() {
+		HeapMover mover = new HeapMover();
+		DRHeapMover(mover, head, 0);
+	}
+
+	private void DRHeapMover(HeapMover mover, Node right, int count) {
+		if (right == null)
+			return;
+		DRHeapMover(mover, right.next, count + 1);
+		if (count >= size / 2) {
+			int temp = mover.left.data;
+			mover.left.data = right.data;
+			right.data = temp;
+		}
+		mover.left = mover.left.next;
+	}
+
+	public int kthFromLast(int idx) throws Exception {
+		Node slow = head;
+		Node fast = head;
+		for (int i = 1; i <= idx; i++)
+			fast = fast.next;
+		while (fast != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+		return slow.data;
+	}
+
+	public void fold() {
+		HeapMover mover = new HeapMover();
+		fold(mover, head, 0);
+	}
+
+	private void fold(HeapMover mover, Node right, int count) {
+		if (right == null)
+			return;
+		fold(mover, right.next, count + 1);
+		Node ahead = null;
+		if (count > size / 2) {
+			ahead = mover.left.next;
+			mover.left.next = right;
+			right.next = ahead;
+
+		}
+		if (count == size / 2) {
+			tail = right;
+			tail.next = null;
+		}
+		mover.left = ahead;
+	}
+
+	public int mid() {
+		Node slow = head;
+		Node fast = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		return slow.data;
+	}
+
+	private Node midNode() {
+		Node slow = head;
+		Node fast = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		return slow;
+	}
+
+	public void kreverse(int k) throws Exception {
+		LinkedList prv = null;
+		while (this.size != 0) {
+			LinkedList cur = new LinkedList();
+			for (int i = 1; i <= k; i++) {
+				cur.addFirst(this.removeFirst());
+			}
+			if (prv == null) {
+				prv = cur;
+			} else {
+				prv.tail.next = cur.head;
+				prv.tail = cur.tail;
+				prv.size += cur.size;
+			}
+
+		}
+		this.head = prv.head;
+		this.tail = prv.tail;
+		this.size = prv.size;
+	}
+
+	public LinkedList mergeTwoSortedLL(LinkedList other) {
+		Node ttemp = this.head;
+		Node otemp = other.head;
+		LinkedList sorted=new LinkedList();
+		while (ttemp != null && otemp != null) {
+           if(ttemp.data<otemp.data)
+           {
+        	   sorted.addLast(ttemp.data);
+        	   ttemp=ttemp.next;
+           }
+           else
+           {
+        	   sorted.addLast(otemp.data);
+        	   otemp=otemp.next;
+           }
+		}
+		while(ttemp!=null)
+		{
+			sorted.addLast(ttemp.data);
+			ttemp=ttemp.next;
+		}
+		while(otemp!=null)
+		{
+			sorted.addLast(otemp.data);
+			otemp=otemp.next;
+		}
+		return sorted;
+	}
+	public void mergeSort()
+	{
+		if(this.size==1)
+			return;
+		Node mid=this.midNode();
+		Node midn=mid.next;
+		LinkedList fh=new LinkedList();
+		fh.head=this.head;
+		fh.tail=mid;
+		fh.tail.next=null;
+		fh.size=(this.size+1)/2;
+		LinkedList sh=new LinkedList();
+		sh.head=midn;
+		sh.tail=this.tail;
+		sh.tail.next=null;
+		sh.size=this.size/2;
+		fh.mergeSort();
+		sh.mergeSort();
+		LinkedList list=fh.mergeTwoSortedLL(sh);
+		this.head=list.head;
+		this.tail=list.tail;
+		this.size=list.size;
+	}
+   public void createdummyLL()
+   {
+	   Node n1=new Node();
+	   n1.data=10;
+	   Node n2=new Node();
+	   n2.data=20;
+	   Node n3=new Node();
+	   n3.data=30;
+	   Node n4=new Node();
+	   n4.data=40;
+	   
+	   Node n5=new Node();
+	   n5.data=50;
+	   Node n6=new Node();
+	   n6.data=60;
+	   n1.next=n2;
+	   n2.next=n3;
+	   n3.next=n4;
+	   n4.next=n5;
+	   n5.next=n6;
+	   n6.next=n3;
+	   this.head=n1;
+	   this.tail=null;
+   }
+   public void removeLoop()
+   {
+	   Node slow=this.head;
+	   Node fast=this.head;
+	   while(fast!=null&&fast.next!=null)
+	   {
+		   slow=slow.next;
+		   fast=fast.next.next;
+		   if(slow==fast)
+			   break;
+	   }
+	   if(slow==fast)
+	   {
+		   Node start=this.head;
+		   while(start.next!=slow.next)
+		   {
+			   start=start.next;
+			   slow=slow.next;
+		   }
+	   }
+	   slow.next=null;
+   }
+}
