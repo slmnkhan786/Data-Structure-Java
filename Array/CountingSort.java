eecbff780b21fc0f35eaebd6e9939a952615b6a4
Salman Khan <36618096+slmnkhan786@users.noreply.github.com>
2019-06-18 12:13:56 +0530

    Add files via upload

---
+++ Array/CountingSort.java
@@ -0,0 +1,55 @@
+package Array;
+
+import java.util.*;
+
+public class CountingSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		int max = -10000000, min = 10000000;
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+			if (max < arr[i])
+				max = arr[i];
+			if (min > arr[i])
+				min = arr[i];
+
+		}
+
+		sort(arr, max, min);
+		print(arr);
+	}
+
+	public static void sort(int arr[], int max, int min) {
+		int count[] = new int[max - min + 1];
+		int output[] = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			count[arr[i] - min]++;
+		}
+
+		for (int i = 1; i < count.length; i++) {
+			count[i] += count[i - 1];
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			output[count[arr[i] - min] - 1] = arr[i];
+			count[arr[i] - min]--;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = output[i];
+		}
+
+	}
+
+	public static void print(int arr[]) {
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+}
