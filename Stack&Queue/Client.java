396201c04b36600ece7cddf13bcbccff7ea2c310
Salman Khan <36618096+slmnkhan786@users.noreply.github.com>
2019-06-24 22:33:28 +0530

    Dynamic Stack Client

--- Stack&Queue/Client.java
+++ Stack&Queue/Client.java
@@ -4,7 +4,7 @@ public class Client {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-        Stack s=new DyStack();
+        DyStack s=new DyStack();
         s.push(10);
         s.push(20);
         s.push(30);
