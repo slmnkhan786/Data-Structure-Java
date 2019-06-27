1afceaf3026c81ea1f1692ffd674a2fb1e576908
Salman Khan <36618096+slmnkhan786@users.noreply.github.com>
2019-06-27 11:59:01 +0530

    Binary Tree Client

--- Tree/BTClient.java
+++ Tree/BTClient.java
@@ -4,7 +4,10 @@ public class BTClient {
 
 	public static void main(String[] args) {
 		// 50 true 40 true 30 false false true 60 false false true 70 false false
+		//		 18 true 10 true 4 false false true 6 false false true 8 true 5 false false true 5 false false
+		
 		BinaryTree bt = new BinaryTree();
+		
 		bt.display();
 		System.out.println(bt.height());
 		System.out.println(bt.max());
@@ -14,8 +17,16 @@ public class BTClient {
 		bt.preOrder();
 		bt.postOrder();
 		bt.inOrder();
-		//bt.levelOrder();
+		bt.levelOrder();
 		bt.levelOrderZZ();
+		bt.rightView();
+		bt.leftView();
+		bt.topView();
+		bt.bottomView();
+		System.out.println(bt.rootToLeafPath(60));
+		bt.lowestCommonAncester(50,60);
+		System.out.println(bt.sumOfNodes());
+		System.out.println(bt.sumTree());
 	}
 
 }
