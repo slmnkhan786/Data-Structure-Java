660f1cb7553e0afa97c6161b2b23f2beb6c261ab
Salman Khan <36618096+slmnkhan786@users.noreply.github.com>
2019-06-28 23:23:11 +0530

    Binary Search Tree Client

---
+++ Tree/BSTClient.java
@@ -0,0 +1,20 @@
+package Tree;
+
+import java.util.*;
+
+public class BSTClient {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
+		BinarySearchTree bst = new BinarySearchTree(arr);
+		bst.display();
+		System.out.println(bst.find(20));
+		System.out.println(bst.max());
+		bst.add(55);
+		bst.add(27);
+		bst.display();
+
+	}
+
+}
